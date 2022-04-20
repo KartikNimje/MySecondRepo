f313f505264655596edcbeec8118546d7d66b80b
Kartik Nimje <kartiknimje1@gmail.com>
2022-04-20 17:28:07 +0530

    Java program for data types

---
+++ HelloJava.java
@@ -0,0 +1,17 @@
+package com.firstprogram;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	       System.out.println("Hello World");
+	        System.out.println(23);
+	        System.out.println(34+56);
+	        System.out.println('H');
+	        System.out.println(true);
+	        
+	
+	}
+
+}
